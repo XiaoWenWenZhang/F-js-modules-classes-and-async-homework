4f201197f1552eb3ba59818990084c85a282b890
xiaowen <846703636@qq.com>
2020-11-15 20:12:52 +0800

    finish practice3

--- src/practice2/Person.js
+++ src/practice2/Person.js
@@ -1,1 +1,11 @@
-export class Person {}
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+}
+
+export default Person;
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,19 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json", { method: "GET" })
+    .then((Response) => {
+      return Response.json();
+    })
+    .then((jsonData) => {
+      const { origin, author, content } = jsonData;
+      const result = [];
+      result.push(origin);
+      result.push(author);
+      result.push(content);
+      return result;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+// export default getPoetry;
